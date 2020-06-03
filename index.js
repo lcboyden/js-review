4f6e2d1af3e4368c5ffdbc141f8827ee01d9b821
Lindsay Boyden <lindsayboyden@yahoo.com>
2020-06-03 13:49:58 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function getTodos() {
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos")
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+}
